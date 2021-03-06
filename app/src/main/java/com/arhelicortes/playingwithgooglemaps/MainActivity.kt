5a50dc0154fa2628b99759be6633d544ca959e3e
ItzTorres <torresitze1996@gmail.com>
2021-03-06 16:47:59 -0600

    Adding Map

--- app/src/main/java/com/arhelicortes/playingwithgooglemaps/MainActivity.kt
+++ app/src/main/java/com/arhelicortes/playingwithgooglemaps/MainActivity.kt
@@ -2,10 +2,18 @@ package com.arhelicortes.playingwithgooglemaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var googleMap: SupportMapFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        googleMap = supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
+        googleMap.getMapAsync {
+
+        }
     }
 }
