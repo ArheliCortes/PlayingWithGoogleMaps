d295fbd82abfdf4f40349fa940ac5ed8d9e9a733
ArheliCortes <arhelicortes2303@gmail.com>
2021-03-13 17:06:04 -0600

    Seguimiento de ubiicacion  y marcadores

--- app/src/main/java/com/arhelicortes/playingwithgooglemaps/MainActivity.kt
+++ app/src/main/java/com/arhelicortes/playingwithgooglemaps/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.arhelicortes.playingwithgooglemaps
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,12 +9,13 @@ import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.arhelicortes.playingwithgooglemaps.viewmodels.MapViewModel
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,7 +24,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var googleMap: SupportMapFragment
     private lateinit var map: GoogleMap
     private val mapViewModel: MapViewModel by viewModels()
+    private var currentMarker: Marker? = null
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,54 +36,67 @@ class MainActivity : AppCompatActivity() {
 
         googleMap.getMapAsync {
             map = it
+            //map.isTrafficEnabled = true
+
+            map.isMyLocationEnabled = true
             mapViewModel.onMapLoaded()
         }
 
         setUpObservers()
     }
 
-    private fun setUpObservers(){
-        mapViewModel.location.observe(this, Observer{
-            val latLng = LatLng(it.latitude,it.longitude)
+    private fun setUpObservers() {
+        mapViewModel.location.observe(this, Observer {
+            val latLng = LatLng(it.latitude, it.longitude)
             map.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(latLng,12f)
+                    CameraUpdateFactory.newLatLngZoom(latLng, 15f)
             )
+            currentMarker?.remove()
+            val markerOptions = MarkerOptions().apply {
+                title("¡Estas aquí!")
+                snippet("Aquí es donde estas")
+                position(latLng)
+            }
+            val marker = map.addMarker(markerOptions)
+            marker.showInfoWindow()
+            currentMarker = marker
+
         })
     }
 
     override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == REQUEST_LOCATION_PERMISSION){
-            if(grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)&& (grantResults[1] == PackageManager.PERMISSION_GRANTED))
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED) && (grantResults[1] == PackageManager.PERMISSION_GRANTED))
                 enableLocation()
         }
     }
 
-    private fun enableLocation(){
-        if(!isPermissionGranted()){
+    private fun enableLocation() {
+        if (!isPermissionGranted()) {
             ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                REQUEST_LOCATION_PERMISSION
+                    this,
+                    arrayOf(
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    REQUEST_LOCATION_PERMISSION
             )
         }
     }
 
-    private fun isPermissionGranted():Boolean {
+    private fun isPermissionGranted(): Boolean {
         return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_COARSE_LOCATION
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
     }
 }
--- app/src/main/java/com/arhelicortes/playingwithgooglemaps/viewmodels/MapViewModel.kt
+++ app/src/main/java/com/arhelicortes/playingwithgooglemaps/viewmodels/MapViewModel.kt
@@ -4,43 +4,64 @@ import android.Manifest
 import android.app.Application
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Looper
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 
-class MapViewModel(val app: Application) : AndroidViewModel(app){
+class MapViewModel(val app: Application) : AndroidViewModel(app) {
 
     private val _location = MutableLiveData<Location>()
     val location: LiveData<Location> = _location
 
-    fun onMapLoaded(){
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            _location.value = locationResult.lastLocation
+        }
+
+    }
+
+
+    fun onMapLoaded() {
         getDeviceLocation()
     }
 
-    private fun getDeviceLocation(){
-        val fusedLocationClient: FusedLocationProviderClient =
-            LocationServices.getFusedLocationProviderClient(app)
+    private fun getDeviceLocation() {
+        fusedLocationClient =
+                LocationServices.getFusedLocationProviderClient(app)
 
-        if(checkPermission()){
+        if (checkPermission()) {
             return
         }
 
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
             _location.value = location
         }
+        val locationRequest = LocationRequest.create().apply {
+            smallestDisplacement = 5f
+            interval = 1000L
+
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
     }
 
-    private fun checkPermission():Boolean{
+    private fun checkPermission(): Boolean {
         return ActivityCompat.checkSelfPermission(
                 app,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 app,
                 Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+        ) != PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+
     }
 }
